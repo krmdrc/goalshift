5d58f2f09be98837f27abbef89536069015e2f09
krmdrc <keremdarici@gmail.com>
2025-11-27 10:22:03 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{
+      width: "100%",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "28px"
+    }}>
+      GoalShift is working!
+    </div>
+  );
+}
